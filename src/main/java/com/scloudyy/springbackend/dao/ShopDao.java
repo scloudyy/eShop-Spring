8dbbdd8b3c6ed211ba406bb48dd735cfca177cb6
scloudyy <onecloud.shen@gmail.com>
2018-12-18 22:50:59 -0800

    feat: add insertShop and updateShop

---
+++ src/main/java/com/scloudyy/springbackend/dao/ShopDao.java
@@ -0,0 +1,10 @@
+package com.scloudyy.springbackend.dao;
+
+import com.scloudyy.springbackend.entity.Shop;
+
+public interface ShopDao {
+
+    int insertShop(Shop shop);
+
+    int updateShop(Shop shop);
+}
---
+++ src/test/java/com/scloudyy/springbackend/dao/ShopDaoTest.java
@@ -0,0 +1,56 @@
+package com.scloudyy.springbackend.dao;
+
+import com.scloudyy.springbackend.BaseTest;
+import com.scloudyy.springbackend.entity.Area;
+import com.scloudyy.springbackend.entity.PersonInfo;
+import com.scloudyy.springbackend.entity.Shop;
+import com.scloudyy.springbackend.entity.ShopCategory;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShopDaoTest extends BaseTest {
+
+    @Autowired
+    private ShopDao shopDao;
+
+    @Test
+    @Ignore
+    public void testInsertShop() {
+        Shop shop = new Shop();
+        PersonInfo owner = new PersonInfo();
+        Area area = new Area();
+        ShopCategory shopCategory = new ShopCategory();
+        owner.setUserId(1L);
+        area.setAreaId(2);
+        shopCategory.setShopCategoryId(1L);
+        shop.setOwner(owner);
+        shop.setArea(area);
+        shop.setShopCategory(shopCategory);
+        shop.setShopName("测试的店铺");
+        shop.setShopDesc("test");
+        shop.setShopAddr("test");
+        shop.setPhone("test");
+        shop.setShopImg("test");
+        shop.setCreateTime(new Date());
+        shop.setEnableStatus(0);
+        shop.setAdvice("审核中");
+        int effectedNum = shopDao.insertShop(shop);
+        assertEquals(1, effectedNum);
+    }
+
+    @Test
+    public void testUpdateShop() {
+        Shop shop = new Shop();
+        shop.setShopId(1L);
+        shop.setShopDesc("测试描述");
+        shop.setShopAddr("测试地址");
+        shop.setLastEditTime(new Date());
+        int effectedNum = shopDao.updateShop(shop);
+        assertEquals(1, effectedNum);
+    }
+}
